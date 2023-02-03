facb2284805c8b2f77c323cf5ccfbcb5d4a0c1cf
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-03 14:01:28 +0100

    add comments and milestone 1

--- index.php
+++ index.php
@@ -11,22 +11,22 @@
 </head>
 <body>
     <?php
-        $back_to_the_Future_I = new Movie('Back to the future', 'sci-fi', '02:12:45', 'part I');
-        $back_to_the_Future_II = new Movie('Back to the future', 'sci-fi', '02:21:21', 'part II');
-        $bullet_train = new Movie('Bullet Train', 'action', '01:48:24');
+        $back_to_the_Future_I = new Movie('Back to the future', ['sci-fi', 'futuristic'], '02:12:45', 'part I');
+        $back_to_the_Future_II = new Movie('Back to the future', ['sci-fi', 'futuristic'], '02:21:21', 'part II');
+        $bullet_train = new Movie('Bullet Train', ['action', 'adrenalinic'], '01:48:24');
         
         echo $back_to_the_Future_I->getTitle().' - ';
         echo $back_to_the_Future_I->getSubtitle().' - ';
-        echo $back_to_the_Future_I->getCategory().' - ';
+        echo $back_to_the_Future_I->getCategoriesToString(', ').' - ';
         echo $back_to_the_Future_I->getDuration().' - ';
         echo '<br>';
         echo $back_to_the_Future_II->getTitle().' - ';
         echo $back_to_the_Future_II->getSubtitle().' - ';
-        echo $back_to_the_Future_II->getCategory().' - ';
+        echo $back_to_the_Future_II->getCategoriesToString(', ').' - ';
         echo $back_to_the_Future_II->getDuration().' - ';
         echo '<br>';
         echo $bullet_train->getTitle().' - ';
-        echo $bullet_train->getCategory().' - ';
+        echo $bullet_train->getCategoriesToString(' / ').' - ';
         echo $bullet_train->getDuration().' - ';
     ?>
 </body>
--- models/Movie.php
+++ models/Movie.php
@@ -3,26 +3,72 @@
 class Movie{
     protected $title;
     protected $subtitle;
-    protected $category;
+    protected $categories = [];
     protected $duration;
 
-    public function __construct($_title, $_category, $_duration , $_subtitle = '')
+    /**
+     * this constructor create a instance of a movie
+     *
+     * @param string $_title
+     * @param Array $_categories
+     * @param string $_duration
+     * @param string $_subtitle
+     */
+    public function __construct($_title, $_categories, $_duration , $_subtitle = '')
     {
         $this->title = $_title;
         $this->subtitle = $_subtitle;
-        $this->category = $_category;
+        $this->categories = $_categories;
         $this->duration = $_duration;
     }
 
+    /**
+     * call this function to get the title of the movie
+     *
+     * @return string the title of the movie
+     */
     public function getTitle(){
         return $this->title;
     }
+
+    /**
+     * call this function to get the subtitle of the movie
+     *
+     * @return string the subtitle of the movie
+     */
     public function getSubtitle(){
         return $this->subtitle;
     }
-    public function getCategory(){
-        return $this->category;
+
+    /**
+     * call this function to get the categories of the movie
+     *
+     * @return Array the categories of the movie
+     */
+    public function getCategories(){
+        return $this->categories;
     }
+
+    /**
+     * call this function to get the categories of the movie in a string divided by text.
+     * Note the divider will not be added at the end of the string
+     *
+     * @param string $dividerString the string to separate each category with
+     * @return string the categories of the movie in a string divided by text
+     */
+    public function getCategoriesToString($dividerString){
+        $str = '';
+        foreach ($this->categories as $key => $category) {
+            $str .= ($key < count($this->categories)-1) ? $category . $dividerString : $category;
+        }
+        return $str;
+    }
+
+    /**
+     * call this function to get the duration of the movie
+     *
+     * @return string the duration of the movie
+     */
     public function getDuration(){
         return $this->duration;
     }
